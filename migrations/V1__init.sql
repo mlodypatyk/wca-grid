49a4d1501bd5e9f489eb413565bef8bf51918b86
Piotr Olszewski <mlodypatyk@gmail.com>
2026-09-10 00:49:37 +0200

    db changes

--- migrations/V1__init.sql
+++ migrations/V1__init.sql
@@ -11,6 +11,5 @@ CREATE TABLE daily_puzzles (
     puzzle_date date PRIMARY KEY,
     puzzle_number int NOT NULL,
     vertical_categories text[] NOT NULL,
-    horizontal_categories text[] NOT NULL,
-    squares jsonb NOT NULL
+    horizontal_categories text[] NOT NULL
 );
--- migrations/V2__seeded_puzzles.sql
+++ migrations/V2__seeded_puzzles.sql
@@ -2,6 +2,5 @@ CREATE TABLE seeded_puzzles (
     seed text PRIMARY KEY,
     vertical_categories text[] NOT NULL,
     horizontal_categories text[] NOT NULL,
-    squares jsonb NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now()
 );
